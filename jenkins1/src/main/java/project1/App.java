88d3c3ca8c7e3dad2a54d01fad64828cc272876e
Karobi Sarkar <karobisarkar323@gmail.com>
2024-05-17 15:13:36 +0530

    commit1

---
+++ jenkins1/src/main/java/project1/App.java
@@ -0,0 +1,12 @@
+package project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" I am Karobi Sarkar. );
+    }
+}
